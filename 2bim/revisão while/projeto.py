2f2ceaba0e13f41a6cb1d695cc2d691ca1ba1fe8
luizzzjuniorrr <penxd11@gmail.com>
2023-09-13 08:41:18 -0300

    130923

---
+++ 2bim/revisão while/projeto.py
@@ -0,0 +1,18 @@
+# while- laço de repetição(3 parametros: contador, condição, atulizar o contador)
+#exemplo1 - mostra os números de 1 até o número informado
+    #num = int(input("Informe um numeor inteiro: "))
+
+    #contador = 1
+
+    #while contador <= num:
+        #print(contador)
+        #contador+=1
+
+#exemplo2 - Mostrar os numeros pares de 0 até o numero informado
+
+num = int(input("Informe um numero: "))
+contador = 0
+
+while contador <= num:
+    print(contador)
+    contador+=2
