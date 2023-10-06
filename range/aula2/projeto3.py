4cf5a1461e8623a8bb87d901fb8cbccfbdb7550a
luizzzjuniorrr <penxd11@gmail.com>
2023-10-06 08:25:56 -0300

    061023

--- range/aula2/projeto3.py
+++ range/aula2/projeto3.py
@@ -1,8 +1,8 @@
 t = int(input("Digite o numero para a tabuada: "))
 s = int(input("Digite o numero de começo da tabuada: "))
 e = int(input("Digite o numero final da tabuada: "))
-
-for i in range(s,e+1):
-    if e>s:
-        print(f"Numero final menor que numero final.")
-    
+if e<s:
+        print(f"Numero final menor que numero inicial.")
+else:
+    for i in range(s,e+1):
+        print(f"{i} x {t} = {t*i}")
