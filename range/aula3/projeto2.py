d6d08d893caa1fcf6dd347ffdf4f323e49f76ace
luizzzjuniorrr <penxd11@gmail.com>
2023-10-09 07:51:33 -0300

    091023

--- range/aula3/projeto2.py
+++ range/aula3/projeto2.py
@@ -1,3 +1,3 @@
 for i in range(5,101):
     if i % 7==0 and i % 5 != 0:
-        print(i)
+        print(f"{i} 7 = {i/7}  5= {i/5}")
---
+++ range/aula4/projeto1.py
@@ -0,0 +1,10 @@
+#informe a quantidade de colunas:
+#informe a quantidade de linhas:
+
+c = int(input("Informe a quantidade de colunas: "))
+l = int(input("Informe a quantidade de linhas: "))
+
+for i in range(c):
+    a=print("*", end='')
+
+print(a)
