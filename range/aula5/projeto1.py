719b4027117badabfcde5810a77efcfce0d1731f
luizzzjuniorrr <penxd11@gmail.com>
2023-10-11 08:35:09 -0300

    111023

---
+++ range/aula5/projeto1.py
@@ -0,0 +1,6 @@
+base= int(input("Digite a base: "))
+exp= int(input("Digite o expoente: "))
+
+for i in range(exp):
+    r=base*base
+print(r)
---
+++ range/aula5/projeto2.py
@@ -0,0 +1,6 @@
+n= int(input("Digite um numero: "))
+r=n
+
+for i in range(1,n):
+    r=r*i
+print(r)
