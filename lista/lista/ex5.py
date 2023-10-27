9de187583d06fef1b47bb0c5f5b365f67c0caccd
luizzzjuniorrr <penxd11@gmail.com>
2023-10-27 08:39:39 -0300

    271023

--- lista/lista/ex5.py
+++ lista/lista/ex5.py
@@ -2,6 +2,6 @@ inicio = int(input("Inicio: "))
 fim = int(input("Fim: "))
 lista = []
 
-for elementos in range(inicio,fim+1):
+for elementos in range(fim,inicio-1,-1):
     lista.append(elementos)
 print(lista)
---
+++ lista/lista/ex6.py
@@ -0,0 +1,5 @@
+quantidade = int(float("Informe a quantidade de números na lista: "))
+lista = []
+
+if i in quantidade:
+    lis
